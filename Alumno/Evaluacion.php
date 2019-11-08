5b5cdb879012cdea351d586e0a73347c2920c430
Leonardo-Colli <44064177+Leonardo-Colli@users.noreply.github.com>
2019-11-07 20:40:15 -0500

    Paginación Preguntas con BDD

--- Alumno/Evaluacion.php
+++ Alumno/Evaluacion.php
@@ -1,3 +1,19 @@
+<?php
+require("conexion.php");
+
+$sql= 'SELECT * FROM preguntas';
+$sentencia = $pdo->prepare($sql);
+$sentencia->execute();
+
+$resultado = $sentencia->fetchAll();
+
+//var_dump($resultado);
+$articulos_x_pagina =1;
+
+$total_preguntas_db =$sentencia->rowCount();
+$paginas = $total_preguntas_db/1; 
+$paginas = ceil($paginas);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,8 +23,28 @@
 	<title>Matematicas</title>
 	<link rel="stylesheet" type="text/css" href="../css/alumn.css">
 	<link rel="stylesheet" type="text/css" href="../css/tools.css">
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+	<?php
+	if(!$_GET){
+		header('Location:Evaluacion.php?pregunta=1');
+	}
+
+	$inciar = ($_GET['pregunta']-1)*$articulos_x_pagina;
+	
+
+	$sql_preguntas='SELECT * FROM preguntas LIMIT :inicar,:npreguntas';
+	$sentencia_preguntas= $pdo->prepare($sql_preguntas);
+	$sentencia_preguntas->bindParam(':inicar', $inciar, PDO::PARAM_INT);
+	$sentencia_preguntas->bindParam(':npreguntas', $articulos_x_pagina, PDO::PARAM_INT);
+	$sentencia_preguntas->execute();
+	$resultado_preguntas= $sentencia_preguntas->fetchAll();
+
+	?>
 	<div class="principal-content flex">
 		<div class="p-content column justify-center">
 			<div class="title justify-center">
@@ -24,37 +60,72 @@
 									</div>
 									<br>
 									<div class="q-box column">
+										<?php foreach ($resultado_preguntas as $articulo):?>
 										<div>
-											<h3>¿Qué pregunta se hará?</h3>
+											<h3>
+												<?php
+												echo $articulo['pregunta'] ?>
+											</h3>
 										</div>
+									<?php endforeach ?>
 										<br>
 										<div class="answers column justify-center">
 											<div class="row">
-												<input type="checkbox" name="">&nbsp; Respuesta
+												<input type="checkbox" name="">&nbsp; <?php echo $articulo['opcion1']?>
 											</div>
 											<br>
 											<div class="row">
-												<input type="checkbox" name="">&nbsp; Respuesta
+												<input type="checkbox" name="">&nbsp;<?php echo $articulo['opcion2']?>
 											</div>
 											<br>
 											<div class="row">
-												<input type="checkbox" name="">&nbsp; Respuesta
+												<input type="checkbox" name="">&nbsp;<?php echo $articulo['opcion3']?>
 											</div>
 											<br>
 											<div class="row">
-												<input type="checkbox" name="">&nbsp; Respuesta
+												<input type="checkbox" name="">&nbsp; <?php echo $articulo['opcion4']?>
 											</div>
+											
 										</div>
+
+
 									</div>
 									<div class="justify-end">
+
 										<style type="text/css"> 
                                     a {color:black;} 
                                     
                                 </style>
-										<a  href="menu.html" style="text-decoration: none" class="btn-question text-center" >Finalizar</a>
+										<a  href="menu.html" style="text-decoration: none" class="btn-question text-center
+
+										" >Finalizar</a>
 									</div>
+
 								</div>	
+								<div class="paginacion">
+									<nav aria-label="Page navigation example">
+										<ul class="pagination">
+											<li class="page-item <?php echo $_GET['pregunta']<=1? 'disabled': ''?>">
+												<a class="page-link" 
+												href="Evaluacion.php?pregunta=<?php echo $_GET['pregunta']-1 ?>">Anterior
+												</a>
+											</li>
+										<?php for($i=0; $i<$paginas;$i++): ?>
+											<li class="page-item <?php echo  $_GET['pregunta']==$i+1 ? 'active': ''?>">
+												<a class="page-link" 
+												href="Evaluacion.php?pregunta=<?php echo $i+1 ?>">
+										<?php echo $i+1 ?>	
+												</a>
+											</li>
+										<?php endfor ?>
+											<li class="page-item<?php echo $_GET['pregunta']>=$paginas? 'disabled': ''?>"><a class="page-link" href="Evaluacion.php?pregunta=<?php echo $_GET['pregunta']+1 ?>">Siguiente
+												</a>
+											</li>
+										</ul>
+									</nav>
+								</div>
 							</div>
+
 						</div>
 				</div>
 			</div>
---
+++ Alumno/conexion.php
@@ -0,0 +1,8 @@
+<?php
+	$servidor = "localhost";
+	$usuario = "root";
+	$senha = "";
+	$dbname = "s_egresados";
+	
+	//Criar a conexao
+	$pdo = new PDO('mysql:host=localhost;dbname=s_egresados', 'root', '');
